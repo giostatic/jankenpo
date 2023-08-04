38e868692f81ada76359f3110109bd5bdd3ee4b5
giostatic <116467840+giostatic@users.noreply.github.com>
2023-08-03 15:15:49 -1000

    created computer choice function

---
+++ main.js
@@ -0,0 +1,8 @@
+let choice = ['rock','paper','scissors']
+
+function getComputerChoice() {
+    let computer = choice[Math.floor(Math.random*choice.length)];
+    return
+}
+
+console.log(getComputerChoice)
