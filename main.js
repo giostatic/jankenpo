b116134fe1803a2153646f7e1acde5e51cc4607a
giostatic <116467840+giostatic@users.noreply.github.com>
2023-08-11 12:12:42 -1000

    completed rock paper scissors game on script

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-let choice = ['rock','paper','scissors']
+const prompt =require("prompt-sync")();
 
-function getComputerChoice() {
-    let computer = choice[Math.floor(Math.random*choice.length)];
-    document.getElementById("button").innerHTML = "YOU CLICKED ME!";
-    return
-}
+let getPerson = prompt('Please select between Rock, Paper, or Scissors:');
 
-console.log(getComputerChoice())
+let choice = ['rock','paper','scissors']; 
+
+function getComputer(arr) {
+    let computer = arr[Math.floor(Math.random() * arr.length)];
+    return computer;
+};
+
+function game(x, y) {
+    if (x == y){
+        return `You selected ${x} and the computer selected ${y}, so you tied!`
+    }
+    if (x == 'rock' && y == 'paper'){
+        return `You selected ${x}, and the computer selected ${y}, you lost!`
+    };
+    if (x == 'rock' && y == 'scissors'){
+        return `You selected ${x}, and the computer selected ${y}, you win!`
+    };
+    if (x == 'paper' && y == 'scissors'){
+        return `You selected ${x}, and the computer selected ${y}, you lost!`
+    };
+    if (x == 'paper' && y == 'rock'){
+        return `You selected ${x}, and the computer selected ${y}, you win!`
+    };
+    if (x == 'scissors' && y == 'rock'){
+        return `You selected ${x}, and the computer selected ${y}, you lost!`
+    };
+    if (x == 'scissors' && y == 'paper'){
+        return `You selected ${x}, and the computer selected ${y}, you win!`
+    };
+
+};
+
+console.log(game(getPerson,getComputer(choice)))
