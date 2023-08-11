fb6a6756bd5adea45ca9d864f761e3ba29a0ca19
giostatic <116467840+giostatic@users.noreply.github.com>
2023-08-11 08:38:04 -1000

    trying to adjust the button to output the text from the program

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ let choice = ['rock','paper','scissors']
 
 function getComputerChoice() {
     let computer = choice[Math.floor(Math.random*choice.length)];
+    document.getElementById("button").innerHTML = "YOU CLICKED ME!";
     return
 }
 
-console.log(getComputerChoice)
+console.log(getComputerChoice())
