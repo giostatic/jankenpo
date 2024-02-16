8956e0cf600de35fbb39b10269394a92f27d774c
giostatic <116467840+giostatic@users.noreply.github.com>
2024-02-15 17:31:46 -1000

    no changes, just syncing

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 const prompt =require("prompt-sync")();
 
-let getPerson = prompt('Please select between Rock, Paper, or Scissors:');
-
 let choice = ['rock','paper','scissors']; 
+
+let player = prompt('Please select between Rock, Paper, or Scissors:');
+
+let getPerson = 
+    do {
+        if(player.toString().toLowerCase() !== 'rock' || player.toString().toLowerCase() !== 'paper'|| player.toString().toLowerCase() !== 'scissors'){
+            return `Input invalid, please select from from the choices provided: ${choice[0]}, ${choice[1]}, or ${choice[2]}`;
+        }else{
+            return;
+        }
+    } while (getPerson);
+
+   // do{
+    //    player = prompt('Please select between Rock, Paper, or Scissors:');
+    //} while (;
+    //while () {
+      //  
+        //player = prompt('Please select between Rock, Paper, or Scissors:');
+    
+    /*prompt === null || prompt === " " || prompt === undefined){
+        return `Input invalid, please select from from the choices provided: ${choice[0]}, ${choice[1]}, or ${choice[2]}`;
+    } else{
+    return};*/
+
 
 function getComputer(arr) {
     let computer = arr[Math.floor(Math.random() * arr.length)];
@@ -10,28 +32,25 @@ function getComputer(arr) {
 };
 
 function game(x, y) {
-    if( x === null || x === "" || x === undefined){
-        return `Input invalid, please select from from the choices provided: ${choice[0]}, ${choice[1]}, or ${choice[2]}`;
-    }
-    if (x.toLowerCase() === y){
+    if (x.toString().toLowerCase() === y){
         return `You selected ${x} and the computer selected ${y}, so you tied!`
     }
-    if (x.toLowerCase() === 'rock' && y === 'paper'){
+    if (x.toString().toLowerCase() === 'rock' && y === 'paper'){
         return `You selected ${x}, and the computer selected ${y}, you lost!`
     };
-    if (x.toLowerCase() === 'rock' && y === 'scissors'){
+    if (x.toString().toLowerCase() === 'rock' && y === 'scissors'){
         return `You selected ${x}, and the computer selected ${y}, you win!`
     };
-    if (x.toLowerCase() === 'paper' && y === 'scissors'){
+    if (x.toString().toLowerCase() === 'paper' && y === 'scissors'){
         return `You selected ${x}, and the computer selected ${y}, you lost!`
     };
-    if (x.toLowerCase() === 'paper' && y === 'rock'){
+    if (x.toString().toLowerCase() === 'paper' && y === 'rock'){
         return `You selected ${x}, and the computer selected ${y}, you win!`
     };
-    if (x.toLowerCase() === 'scissors' && y === 'rock'){
+    if (x.toString().toLowerCase() === 'scissors' && y === 'rock'){
         return `You selected ${x}, and the computer selected ${y}, you lost!`
     };
-    if (x.toLowerCase() === 'scissors' && y === 'paper'){
+    if (x.toString().toLowerCase() === 'scissors' && y === 'paper'){
         return `You selected ${x}, and the computer selected ${y}, you win!`
     };
 
